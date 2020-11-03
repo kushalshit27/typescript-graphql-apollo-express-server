3dcade4bafc36bc59c2a3fbdc05f486f8269b76d
unknown <kushalshit27@gmail.com>
2020-11-03 11:03:45 +0530

    code update

--- app/server.ts
+++ app/server.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
-import { connect } from "mongoose";
+//import { connect } from "mongoose";
 
 
 import {UserResolver} from "./resolvers/User";
@@ -25,19 +25,19 @@ const schema = await buildSchema({
 
 
 // create mongoose connection
-const mongoose = await connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
-await mongoose.connection;
+// const mongoose = await connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+// await mongoose.connection;
   
 
 
-const server = new ApolloServer({schema});
+const server = new ApolloServer({schema}) as any;
 
-const app = Express();
+const expressApp : Express.Application = Express();
 
-server.applyMiddleware({app});
+server.applyMiddleware({app: expressApp, path: '/graphql'});
 
-app.listen({ port: 3333 }, () =>
-  console.log(`🚀 Server ready and listening at ==> http://localhost:3333${server.graphqlPath}`))
+expressApp.listen({ port: 3000 }, () =>
+  console.log(`🚀 Server ready and listening at ==> http://localhost:3000${server.graphqlPath}`))
 
 };
 
